88a4a790a90ff3e6c8e40fdfad0e9db6e0254c37
unilock <unilock@fennet.rentals>
2023-08-15 10:41:04 -0400

    Update to 1.19.2 + Replace mixin with ServerMessageEvents.ALLOW_CHAT_MESSAGE
    Signed-off-by: unilock <unilock@fennet.rentals>

--- src/main/java/dev/hugeblank/Allium.java
+++ src/main/java/dev/hugeblank/Allium.java
@@ -1,7 +1,10 @@
 package dev.hugeblank;
 
 import dev.hugeblank.peripherals.chatmodem.BlockChatModem;
+import dev.hugeblank.peripherals.chatmodem.ChatModemState;
+import dev.hugeblank.peripherals.chatmodem.IChatCatcher;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.message.v1.ServerMessageEvents;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.block.Material;
@@ -17,6 +20,21 @@ public class Allium implements ModInitializer {
         AlliumRegistry.registerBlocks();
         AlliumRegistry.registerBlockEntities();
         AlliumRegistry.registerItems();
+
+        ServerMessageEvents.ALLOW_CHAT_MESSAGE.register((message, sender, params) -> {
+            boolean cancel = false;
+            Allium.debug( "Catchers: " + IChatCatcher.CATCHERS);
+            for (ChatModemState modem : IChatCatcher.CATCHERS) {
+                if (modem.creative || modem.isBound() && sender.getUuid().equals(modem.getBound().uuid())) {
+                    boolean c = modem.handleChatEvents(message.getContent().getString(), sender);
+                    if (c) cancel = true;
+                    Allium.debug("World: server, cancelled: " + (cancel ? "yes" : "no"));
+                } else if (!modem.isBound()) { // This should never happen.
+                    Allium.debug("Modem " + modem + " is registered as a handler, but has no bound player");
+                }
+            }
+            return !cancel;
+        });
     }
 
     public static void debug(Object o) {
